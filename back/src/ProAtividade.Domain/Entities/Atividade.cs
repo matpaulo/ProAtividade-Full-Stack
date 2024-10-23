e5038576ab57e03ee64127c2deb150084e3da4c9
matpaulo <matheus.locama2015@gmail.com>
2024-10-23 12:44:55 -0300

    commit domain

---
+++ back/src/ProAtividade.Domain/Entities/Atividade.cs
@@ -0,0 +1,30 @@
+namespace ProAtividade.Domain.Entities
+{
+    public class Atividade
+    {
+        public int Id {get; set;}
+        public string? Titulo {get; set;}
+        public string? Descricao {get; set;}
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataConclusao { get; set; }
+        public Prioridade Prioridade { get; set; }
+
+        public Atividade() => DataCriacao = DateTime.Now;
+        public Atividade(int id, string titulo, string descricao) : this()
+        {
+            Id = id;
+            Titulo = titulo;
+            Descricao = descricao;
+        }
+
+        public void Concluir()
+        {
+            if (DataConclusao == null) {
+                DataConclusao = DateTime.Now;
+            } else {
+                throw new Exception($"Atividade já concluida em {DataConclusao.ToString("dd/mm/yyyy hh:mm")}");
+            }
+                
+        }
+    }
+}
